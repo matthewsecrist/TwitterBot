51ce7677c070a60281e94e4dc5aae4acedaf7f46
Matthew Secrist <matthew.secrist@outlook.com>
2017-07-23 22:01:44 -0500

    Remove old comments.

--- lib/TwitterBot/ai.ex
+++ lib/TwitterBot/ai.ex
@@ -1,8 +1,7 @@
 defmodule TwitterBot.Ai do
   @headers ["Authorization": "Bearer #{Application.get_env(:twitter_bot, :wit_key)}", "Accept": "Application/json; Charset=utf-8"]
-  @doc """
-    fetch(user, project) to contact the github API and then handle the response.
-  """
+
+
   def fetch(text) do
     text
     |> String.replace(" ", "%20")
@@ -12,21 +11,12 @@ defmodule TwitterBot.Ai do
     |> decode_response
   end
 
-  # Returns the Issues URL
   def wit_url(text) do
     "https://api.wit.ai/message?v=20170723&q='#{text}'"
   end
 
   def handle_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
     {:ok, Poison.decode!(body)}
-  end
-
-  def handle_response({:ok, %HTTPoison.Response{status_code: 404}}) do
-   IO.puts "Not Found :("
-  end
-
-  def handle_response({:error, %HTTPoison.Error{reason: reason}}) do
-    IO.inspect reason
   end
 
   def decode_response({:ok, body}) do
